1b76fe9c2911ce5778a96497e75b7b3479fa9229
aaronl2026 <aaronl2026@student.cis.edu.hk>
2023-09-12 18:48:26 +0800

    Amendments to Lab 1.1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,11 @@
-public class Dateable {
+import java.util.Scanner;
 
+public class Dateable {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        int minimum_age = 7+(age/2);
+        System.out.println(age+"-year olds should date somebody who is at least "+minimum_age+" years old.");
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter your age: ");
+        int age = scanner.nextInt();
+        int number_of_pages = 100 - age;
+        System.out.println(age+"-year olds should read at least "+number_of_pages+" pages before giving up on a book.");
     }
 }
